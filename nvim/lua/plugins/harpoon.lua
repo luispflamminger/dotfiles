db89d8153176e3a84d45f0423b8b260e58c8a0dc
Luis Pflamminger <luis.pflamminger@gmail.com>
2024-01-25 09:30:44 +0100

    fix: fix harpoon configuration

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,3 +1,4 @@
+
 return {
     {
         "ThePrimeagen/harpoon",
@@ -8,20 +9,15 @@ return {
         },
         config = function()
             local harpoon = require("harpoon")
-
             harpoon.setup()
 
-            vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
+            vim.keymap.set("n", "<leader>h", function() harpoon:list():append() end)
             vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
             vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
             vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
             vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
-
-            -- Toggle previous & next buffers stored within Harpoon list
-            -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-            -- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+            vim.keymap.set('n', '<C-;>', function() harpoon:list():select(4) end)
         end
     }
 }
