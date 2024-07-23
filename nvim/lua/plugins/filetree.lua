94a988dd994e8266941b322353135f3f1aeeab1f
Luis Pflamminger <luis.pflamminger@gmail.com>
2024-07-23 18:16:32 +0200

    feat(nvim): add keybinds for filetree

--- nvim/lua/plugins/filetree.lua
+++ nvim/lua/plugins/filetree.lua
@@ -2,13 +2,23 @@ return {
     {
         "nvim-tree/nvim-tree.lua",
         dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {
-            renderer = { group_empty = true, },
-        },
-        init = function()
+        config = function()
+            -- disable netrw
             vim.g.loaded_netrw = 1
             vim.g.loaded_netrwPlugin = 1
+
+            -- plugin configuration
+            local opts = {
+                renderer = { group_empty = true, },
+            }
+
+            -- configure
+            require("nvim-tree").setup(opts)
+
+            -- keybinds
+            vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<cr>")
+            vim.keymap.set("n", "<leader>tf", "<cmd>NvimTreeFocus<cr>")
+            vim.keymap.set("n", "<leader>tf", "<cmd>NvimTreeFindFile<cr>")
         end,
-        config = true,
     },
 }
