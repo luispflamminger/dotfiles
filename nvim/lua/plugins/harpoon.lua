13cc8c084b2ccbd1db2bb38b459531fdce505f0f
Luis Pflamminger <luis.pflamminger@gmail.com>
2024-07-24 21:42:53 +0200

    chore(nvim): remove deprecated harpoon api call

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -11,7 +11,7 @@ return {
             local harpoon = require("harpoon")
             harpoon.setup()
 
-            vim.keymap.set("n", "<leader>h", function() harpoon:list():append() end)
+            vim.keymap.set("n", "<leader>h", function() harpoon:list():add() end)
             vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
             vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
