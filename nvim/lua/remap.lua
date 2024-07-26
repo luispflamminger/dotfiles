a210db907b3086e7d3d53f44d7c238b865d33254
Luis Pflamminger <luis.pflamminger@gmail.com>
2024-07-26 12:55:38 +0200

    chore: remove unnecessary binds

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -5,8 +5,6 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- yank to clipboard
 vim.keymap.set("", "<leader>y", "\"+y")
-vim.keymap.set("", "<leader>y", "\"+y")
-vim.keymap.set("", "<leader>Y", "\"+Y")
 
 -- Paste over selection without replacing register contents
 vim.keymap.set("x", "<leader>p", [["_dP]])
