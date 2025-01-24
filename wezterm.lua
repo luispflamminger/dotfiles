ff77009659cf017d21bac9d2409a5f78043f223e
Luis Pflamminger <luis.pflamminger@gmail.com>
2025-01-24 17:39:12 +0100

    chore(wezterm): change keybindings

--- wezterm.lua
+++ wezterm.lua
@@ -11,8 +11,20 @@ return {
     -- Run wsl if on windows
     default_prog = default_prog,
 
-    -- Disable default key bindings
+    -- Set key bindings explicitly
     disable_default_key_bindings = true,
+    keys = {
+        { -- copy to clipboard
+            key = "c",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action { CopyTo = "Clipboard" },
+        },
+        { -- paste from clipboard
+            key = "v",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action { PasteFrom = "Clipboard" },
+        },
+    },
 
     -- Hide tab bar when there's only one tab
     hide_tab_bar_if_only_one_tab = true,
