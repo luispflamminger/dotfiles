cb82db3dfd6180664a8d8293bd5e095f53d7dc54
Luis Pflamminger <luis.pflamminger@gmail.com>
2025-01-24 16:00:21 +0100

    feat(nvim): add markdown rendering

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -55,4 +55,11 @@ return {
             scope = { enabled = false },
         },
     },
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    }
 }
