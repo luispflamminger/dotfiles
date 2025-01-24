af3ae2898d8f9d7d2a61dcf66f4fb8b01abdd9dc
Luis Pflamminger <luis.pflamminger@gmail.com>
2025-01-24 16:09:33 +0100

    chore(nvim): configure md renderer

--- nvim/lua/plugins/appearance.lua
+++ nvim/lua/plugins/appearance.lua
@@ -58,8 +58,9 @@ return {
     {
         'MeanderingProgrammer/render-markdown.nvim',
         dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-        ---@module 'render-markdown'
-        ---@type render.md.UserConfig
-        opts = {},
-    }
+        opts = {
+            file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+    },
 }
