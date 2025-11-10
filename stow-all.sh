6efc257c4ab91f8241c5f15812510c17247c7e52
Luis Pflamminger <luis.pflamminger@gmail.com>
2025-11-10 08:52:12 +0100

    chore: add wallpaper

--- stow-all.sh
+++ stow-all.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -euo pipefail
 for dir in *; do
-  if [[ ! -d "$dir" ]] || [[ "$dir" == "disabled" ]]; then
+  if [[ ! -d "$dir" ]] || [[ "$dir" == "disabled" ]] || [[ "$dir" == "assets" ]]; then
     # Skip if not a directory or if it's named "disabled"
     continue
   fi
-    echo "Stowing dotfiles from: $dir"
-    stow --dotfiles "$dir"
+  echo "Stowing dotfiles from: $dir"
+  stow --dotfiles "$dir"
 done
